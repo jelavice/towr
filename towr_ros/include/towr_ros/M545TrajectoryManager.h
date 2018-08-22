9f22aa18972ee665adbc4d95e3773bd9de29838f
jelavice <edo.jelavic@gmail.com>
2018-08-22 09:52:27 +0200

    Switch to the new message type

--- towr_ros/include/towr_ros/M545TrajectoryManager.h
+++ towr_ros/include/towr_ros/M545TrajectoryManager.h
@@ -17,10 +17,12 @@
 #include <towr/terrain/height_map.h>
 #include <towr/variables/spline_holder.h>
 #include <xpp_states/robot_state_cartesian.h>
-#include <xpp_states/robot_state_joint.h>
+#include <xpp_states/robot_state_cartesian_plus_joints.h>
 
 #include <towr_ros/topic_names.h>
 #include <towr_ros/towr_xpp_ee_map.h>
+
+#include <towr/variables/euler_converter.h>
 
 namespace towr {
 
@@ -30,7 +32,7 @@ class M545TrajectoryManager
  public:
 
   using XppVecCartesian = std::vector<xpp::RobotStateCartesian>;
-  using XppVecJoints = std::vector<xpp::RobotStateJoint>;
+  using XppVecJoints = std::vector<xpp::RobotStateCartesianPlusJoints>;
   using Vector3d = Eigen::Vector3d;
 
   M545TrajectoryManager(const HeightMap *terrain);
@@ -42,9 +44,13 @@ class M545TrajectoryManager
 
   XppVecJoints GetTrajectoryJoints(const SplineHolder &solution);
   void SaveTrajectoryInRosbagJoints(rosbag::Bag& bag, const std::string& topic,
-                                       const SplineHolder &solution);
+                                    const SplineHolder &solution);
 
  private:
+
+  void FillCartesianState(double t, int n_ee, const SplineHolder &solution,
+                                                 EulerConverter &base_angular,
+                                                 xpp::RobotStateCartesian &state);
 
   const double visualization_dt_ = 0.1;
   const HeightMap *terrain_ = nullptr;
--- towr_ros/src/M545TrajectoryManager.cc
+++ towr_ros/src/M545TrajectoryManager.cc
@@ -6,12 +6,9 @@
  */
 
 #include <towr_ros/M545TrajectoryManager.h>
-#include <towr/variables/euler_converter.h>
 #include <xpp_msgs/RobotStateCartesian.h>
-#include <xpp_msgs/RobotStateJoint.h>
+#include <xpp_msgs/RobotStateCartesianPlusJoints.h>
 #include <xpp_states/convert.h>
-
-
 
 namespace towr {
 
@@ -19,6 +16,30 @@ M545TrajectoryManager::M545TrajectoryManager(const HeightMap *terrain)
     : terrain_(terrain)
 {
 
+}
+
+void M545TrajectoryManager::FillCartesianState(double t, int n_ee, const SplineHolder &solution,
+                                               EulerConverter &base_angular,
+                                               xpp::RobotStateCartesian &state)
+{
+
+  state.base_.lin = ToXpp(solution.base_linear_->GetPoint(t));
+
+  state.base_.ang.q = base_angular.GetQuaternionBaseToWorld(t);
+  state.base_.ang.w = base_angular.GetAngularVelocityInWorld(t);
+  state.base_.ang.wd = base_angular.GetAngularAccelerationInWorld(t);
+
+  for (int ee_towr = 0; ee_towr < n_ee; ++ee_towr) {
+    int ee_xpp = ToXppEndeffector(n_ee, ee_towr).first;
+
+    state.ee_contact_.at(ee_xpp) = solution.phase_durations_.at(ee_towr)->IsContactPhase(t);
+    state.ee_motion_.at(ee_xpp) = ToXpp(solution.ee_motion_.at(ee_towr)->GetPoint(t));
+    state.ee_forces_.at(ee_xpp) = solution.ee_force_.at(ee_towr)->GetPoint(t).p();
+    //state.wheel_angles_.at(ee_xpp) = solution.ee_wheel_angles_.at(ee_towr)->GetPoint(t).p()(0);
+    //std::cout << "ee_xpp: " << ee_xpp << "/" << n_ee << " : " << solution.ee_motion_.at(ee_towr)->GetPoint(t).p().transpose() << std::endl;
+  }
+
+  state.t_global_ = t;
 }
 
 M545TrajectoryManager::XppVecCartesian M545TrajectoryManager::GetTrajectoryCartesian(
@@ -32,26 +53,12 @@ M545TrajectoryManager::XppVecCartesian M545TrajectoryManager::GetTrajectoryCarte
   EulerConverter base_angular(solution.base_angular_);
 
   while (t <= T + 1e-5) {
+
     int n_ee = solution.ee_motion_.size();
     xpp::RobotStateCartesian state(n_ee);
 
-    state.base_.lin = ToXpp(solution.base_linear_->GetPoint(t));
+    FillCartesianState(t, n_ee, solution, base_angular, state);
 
-    state.base_.ang.q = base_angular.GetQuaternionBaseToWorld(t);
-    state.base_.ang.w = base_angular.GetAngularVelocityInWorld(t);
-    state.base_.ang.wd = base_angular.GetAngularAccelerationInWorld(t);
-
-    for (int ee_towr = 0; ee_towr < n_ee; ++ee_towr) {
-      int ee_xpp = ToXppEndeffector(n_ee, ee_towr).first;
-
-      state.ee_contact_.at(ee_xpp) = solution.phase_durations_.at(ee_towr)->IsContactPhase(t);
-      state.ee_motion_.at(ee_xpp) = ToXpp(solution.ee_motion_.at(ee_towr)->GetPoint(t));
-      state.ee_forces_.at(ee_xpp) = solution.ee_force_.at(ee_towr)->GetPoint(t).p();
-      //state.wheel_angles_.at(ee_xpp) = solution.ee_wheel_angles_.at(ee_towr)->GetPoint(t).p()(0);
-      //std::cout << "ee_xpp: " << ee_xpp << "/" << n_ee << " : " << solution.ee_motion_.at(ee_towr)->GetPoint(t).p().transpose() << std::endl;
-    }
-
-    state.t_global_ = t;
     trajectory.push_back(state);
     t += visualization_dt_;
 
@@ -60,66 +67,31 @@ M545TrajectoryManager::XppVecCartesian M545TrajectoryManager::GetTrajectoryCarte
   return trajectory;
 }
 
-//todo fix copy paste
+//todo fix the code copy paste
 M545TrajectoryManager::XppVecJoints M545TrajectoryManager::GetTrajectoryJoints(
     const SplineHolder &solution)
 {
 
-  //todo not use the joint class at all it is restrictive for what I need
   XppVecJoints trajectory;
   double t = 0.0;
   double T = solution.base_linear_->GetTotalTime();
 
   EulerConverter base_angular(solution.base_angular_);
 
-  // find the trajecotry with
-  int maxJointNum = 0;
-  for (int i = 0; i < solution.joint_motion_.size(); ++i) {
-    if (solution.joint_motion_.at(i)->GetPoint(0).p().size() > maxJointNum)
-      maxJointNum = solution.joint_motion_.at(i)->GetPoint(0).p().size();
-  }
-
   while (t <= T + 1e-5) {
-    int n_ee = solution.joint_motion_.size();
-    //
-    xpp::RobotStateJoint state(n_ee, maxJointNum);
 
-    state.base_.lin = ToXpp(solution.base_linear_->GetPoint(t));
+    int n_ee = solution.ee_motion_.size();
+    xpp::RobotStateCartesianPlusJoints state(n_ee);
 
-    state.base_.ang.q = base_angular.GetQuaternionBaseToWorld(t);
-    state.base_.ang.w = base_angular.GetAngularVelocityInWorld(t);
-    state.base_.ang.wd = base_angular.GetAngularAccelerationInWorld(t);
+    FillCartesianState(t, n_ee, solution, base_angular, state);
 
-    Eigen::VectorXd all_ee_joints(n_ee * maxJointNum), all_zeros(n_ee * maxJointNum);
-    all_zeros.setZero();
-    all_ee_joints.setZero();
-    int count = 0;
 
-    // copy the contacts and stack all joints ito a vector
-    for (int ee_towr = 0; ee_towr < n_ee; ++ee_towr) {
-      int ee_xpp = ToXppEndeffector(n_ee, ee_towr).first;
-
-      state.ee_contact_.at(ee_xpp) = solution.phase_durations_.at(ee_towr)->IsContactPhase(t);
-
-      // now actually copy the useful values and skip the excess num of joints
-      // damn you alex and your stupid joint class
-      Eigen::VectorXd temp = solution.joint_motion_.at(ee_towr)->GetPoint(t).p();
-      for (int i = 0; i < maxJointNum; ++i)
-        if (i < temp.size())
-          all_ee_joints(count++) = temp(i);
-        else
-          ++count;
-
+    //now also fill in the joints
+    for (int i = 0; i < n_ee; ++i){
+      state.joint_positions_.at(i) = solution.joint_motion_.at(i)->GetPoint(t).p();
     }
 
-    state.q_.SetFromVec(all_ee_joints);
 
-    // set the velocities and the torques to zero
-    state.qd_.SetFromVec(all_zeros);
-    state.qdd_.SetFromVec(all_zeros);
-    state.torques_.SetFromVec(all_zeros);
-
-    state.t_global_ = t;
     trajectory.push_back(state);
     t += visualization_dt_;
 
@@ -135,73 +107,59 @@ void M545TrajectoryManager::SaveTrajectoryInRosbagCartesian(rosbag::Bag& bag,
 
   auto traj = GetTrajectoryCartesian(solution);
 
-    for (const auto state : traj) {
-      auto timestamp = ::ros::Time(state.t_global_ + 1e-6);  // t=0.0 throws ROS exception
+  for (const auto state : traj) {
+    auto timestamp = ::ros::Time(state.t_global_ + 1e-6);  // t=0.0 throws ROS exception
 
-      xpp_msgs::RobotStateCartesian msg;
-      msg = xpp::Convert::ToRos(state);
+    xpp_msgs::RobotStateCartesian msg;
+    msg = xpp::Convert::ToRos(state);
 
-      //todo remove the hack
-      //    for (auto wheel_angle : state.wheel_angles_.ToImpl())
-      //      msg.wheel_angles.push_back(wheel_angle);
+    //todo remove the hack
+    //    for (auto wheel_angle : state.wheel_angles_.ToImpl())
+    //      msg.wheel_angles.push_back(wheel_angle);
 
-      bag.write(topic, timestamp, msg);
+    bag.write(topic, timestamp, msg);
 
-      xpp_msgs::TerrainInfo terrain_msg;
-      for (auto ee : state.ee_motion_.ToImpl()) {
-        Vector3d n = terrain_->GetNormalizedBasis(HeightMap::Normal, ee.p_.x(), ee.p_.y());
-        terrain_msg.surface_normals.push_back(xpp::Convert::ToRos<geometry_msgs::Vector3>(n));
-        terrain_msg.friction_coeff = terrain_->GetFrictionCoeff();
-      }
-
-      bag.write(xpp_msgs::terrain_info, timestamp, terrain_msg);
+    xpp_msgs::TerrainInfo terrain_msg;
+    for (auto ee : state.ee_motion_.ToImpl()) {
+      Vector3d n = terrain_->GetNormalizedBasis(HeightMap::Normal, ee.p_.x(), ee.p_.y());
+      terrain_msg.surface_normals.push_back(xpp::Convert::ToRos<geometry_msgs::Vector3>(n));
+      terrain_msg.friction_coeff = terrain_->GetFrictionCoeff();
     }
+
+    bag.write(xpp_msgs::terrain_info, timestamp, terrain_msg);
+  }
 
 }
 
 //todo fix code copy paste
-void M545TrajectoryManager::SaveTrajectoryInRosbagJoints(rosbag::Bag& bag,
-                                                            const std::string& topic,
-                                                            const SplineHolder &solution)
+void M545TrajectoryManager::SaveTrajectoryInRosbagJoints(rosbag::Bag& bag, const std::string& topic,
+                                                         const SplineHolder &solution)
 {
+
+
+
 
   auto traj = GetTrajectoryJoints(solution);
 
-  for (const auto state : traj) {
-    auto timestamp = ::ros::Time(state.t_global_ + 1e-6);  // t=0.0 throws ROS exception
+    for (const auto state : traj) {
+      auto timestamp = ::ros::Time(state.t_global_ + 1e-6);  // t=0.0 throws ROS exception
 
-    xpp_msgs::RobotStateJoint msg;
+      xpp_msgs::RobotStateCartesianPlusJoints msg;
+      msg = xpp::Convert::ToRos(state);
 
-    msg.base = xpp::Convert::ToRos(state.base_);
 
-        //convert the joints too
-    Eigen::VectorXd jointVec = state.q_.ToVec();
-    for (int i = 0; i < jointVec.size(); ++i){
-      msg.joint_state.name.push_back("");
-      msg.joint_state.position.push_back(jointVec(i));
-      msg.joint_state.velocity.push_back(0.0);
-      msg.joint_state.effort.push_back(0.0);
+      bag.write(topic, timestamp, msg);
 
+//      xpp_msgs::TerrainInfo terrain_msg;
+//      for (auto ee : state.ee_motion_.ToImpl()) {
+//        Vector3d n = terrain_->GetNormalizedBasis(HeightMap::Normal, ee.p_.x(), ee.p_.y());
+//        terrain_msg.surface_normals.push_back(xpp::Convert::ToRos<geometry_msgs::Vector3>(n));
+//        terrain_msg.friction_coeff = terrain_->GetFrictionCoeff();
+//      }
+//
+//      bag.write(xpp_msgs::terrain_info, timestamp, terrain_msg);
     }
 
-    //copy the joint contact info
-    for (int i =0; i < state.ee_contact_.GetEECount(); ++i)
-      msg.ee_contact.push_back(state.ee_contact_.at(i));
-
-
-    bag.write(topic, timestamp, msg);
-
-    //todo just stick the trajectory in the the cartesian state this shit with joints is annoying
-
-//    xpp_msgs::TerrainInfo terrain_msg;
-//    for (auto ee : state.ee_motion_.ToImpl()) {
-//      Vector3d n = terrain_->GetNormalizedBasis(HeightMap::Normal, ee.p_.x(), ee.p_.y());
-//      terrain_msg.surface_normals.push_back(xpp::Convert::ToRos<geometry_msgs::Vector3>(n));
-//      terrain_msg.friction_coeff = terrain_->GetFrictionCoeff();
-//    }
-//
-//    bag.write(xpp_msgs::terrain_info, timestamp, terrain_msg);
-  }
 
 }
 
