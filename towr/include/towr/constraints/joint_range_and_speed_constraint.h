38fcbd0179f99cb0771f7bb47e3b6df4618bd6f9
jelavice <edo.jelavic@gmail.com>
2018-08-23 20:04:59 +0200

    Adding the joint range and velocity constraints

---
+++ towr/include/towr/constraints/joint_range_and_speed_constraint.h
@@ -0,0 +1,60 @@
+/*
+ * range_of_motion_constraint_complicated.h
+ *
+ *  Created on: Aug 2, 2018
+ *      Author: jelavice
+ */
+
+#pragma once
+
+#include <towr/variables/spline.h>
+#include <towr/variables/spline_holder.h>
+#include <towr/variables/euler_converter.h>
+
+#include <towr/models/kinematic_model_joints.h>
+#include <towr/models/kinematic_model.h>
+#include "time_discretization_constraint.h"
+
+namespace towr {
+
+class JointRangeAndSpeedConstraint : public TimeDiscretizationConstraint
+{
+ public:
+  using EE = uint;
+  using Vector3d = Eigen::Vector3d;
+  using SparseVector = Eigen::SparseVector<double,Eigen::RowMajor>;
+
+  JointRangeAndSpeedConstraint(KinematicModelJoints::Ptr robot_model, double T, double dt,
+                                EE ee, const SplineHolder& spline_holder);
+
+  ~JointRangeAndSpeedConstraint() = default;
+
+ private:
+
+  const double max_joint_vel_ = 0.5; /* rad/s */
+
+  int num_dof_;
+  EE ee_;
+
+  NodeSpline::Ptr joints_motion_;
+
+  int num_constraints_per_node_;
+
+  KinematicModelJoints::Ptr kinematic_model_;
+
+  Eigen::VectorXd lower_bounds_;
+  Eigen::VectorXd upper_bounds_;
+
+  // see TimeDiscretizationConstraint for documentation
+  void UpdateConstraintAtInstance(double t, int k, VectorXd& g) const final;
+  void UpdateBoundsAtInstance(double t, int k, VecBound& bounds) const final;
+  void UpdateJacobianAtInstance(double t, int k, std::string var_set, Jacobian&) const final;
+
+  int GetRow(int node) const;
+
+
+
+};
+
+}
+/* namesapce*/
---
+++ towr/src/joint_range_and_speed_constraint.cc
@@ -0,0 +1,75 @@
+/*
+ * joint_range_and_speed_constraint.cc
+ *
+ *  Created on: Aug 23, 2018
+ *      Author: jelavice
+ */
+
+#include "towr/constraints/joint_range_and_speed_constraint.h"
+#include <towr/variables/variable_names.h>
+
+namespace towr {
+
+JointRangeAndSpeedConstraint::JointRangeAndSpeedConstraint(KinematicModelJoints::Ptr model,
+                                                           double T, double dt, EE ee,
+                                                           const SplineHolder& spline_holder)
+    : TimeDiscretizationConstraint(T, dt, "jointrangeandmotion-" + std::to_string(ee))
+{
+
+  ee_ = ee;
+
+  joints_motion_ = spline_holder.joint_motion_.at(ee_);
+
+  kinematic_model_ = model;
+  num_dof_ = kinematic_model_->GetNumDof(ee_);
+
+  num_constraints_per_node_ = 2 * kinematic_model_->GetNumDof(ee_);  //for the range and for the velocity
+
+  //cache these guys
+  lower_bounds_ = kinematic_model_->GetLowerJointLimits(ee_);
+  upper_bounds_ = kinematic_model_->GetUpperJointLimits(ee_);
+
+  SetRows(GetNumberOfNodes() * num_constraints_per_node_);
+}
+
+void JointRangeAndSpeedConstraint::UpdateConstraintAtInstance(double t, int k, VectorXd& g) const
+{
+  int row_start = GetRow(k);
+
+  g.middleRows(row_start, num_dof_) = joints_motion_->GetPoint(t).p();
+  row_start += num_dof_;
+  g.middleRows(row_start, num_dof_) = joints_motion_->GetPoint(t).v();
+
+}
+void JointRangeAndSpeedConstraint::UpdateBoundsAtInstance(double t, int k, VecBound& bounds) const
+{
+  int row_start = GetRow(k);
+  for (int i = 0; i < num_dof_; ++i) {
+    bounds.at(row_start + i) = ifopt::Bounds(lower_bounds_(i), upper_bounds_(i));  //update joint position bounds
+    bounds.at(row_start + i + num_dof_) = ifopt::Bounds(-max_joint_vel_, max_joint_vel_);  // update joint velocity bounds
+  }
+
+}
+void JointRangeAndSpeedConstraint::UpdateJacobianAtInstance(double t, int k, std::string var_set,
+                                                            Jacobian& jac) const
+{
+
+  if (var_set == id::JointNodes(ee_)) {
+    int row_start = GetRow(k);
+
+    jac.middleRows(row_start, num_dof_) = joints_motion_->GetJacobianWrtNodes(t, kPos);
+    row_start += num_dof_;
+    jac.middleRows(row_start, num_dof_) = joints_motion_->GetJacobianWrtNodes(t, kVel);
+
+  }
+
+}
+
+int JointRangeAndSpeedConstraint::GetRow(int node) const
+{
+  return node * num_constraints_per_node_;
+
+}
+
+} /* namespace */
+
