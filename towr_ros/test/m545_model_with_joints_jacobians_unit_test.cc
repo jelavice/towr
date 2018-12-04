d6e0021a54b8bcc488f61e5cccb6825bcbbb8ef6
jelavice <edo.jelavic@gmail.com>
2018-12-04 17:24:28 +0100

    clean up the test

--- towr_ros/test/m545_model_with_joints_jacobians_unit_test.cc
+++ towr_ros/test/m545_model_with_joints_jacobians_unit_test.cc
@@ -60,110 +60,105 @@ Eigen::MatrixXd getJacobianNumerically(M545KinematicModelWithJoints* kinematic_m
 
 }
 
+void runTest(const ros::NodeHandle &nh, const std::string &which_test, int numTests ){
+
+  //get the excavator model
+    std::string urdfDescription;
+    nh.getParam("romo_mm_description", urdfDescription);
+
+    constexpr double dt = 0.01;
+    auto kinematic_model = std::make_shared<M545KinematicModelWithJoints>(urdfDescription, dt);
+
+
+    std::vector<Eigen::MatrixXd> ee_jac_base_num;
+    M545KinematicModelWithJoints::EEJac ee_jac_base;
+    std::vector<Eigen::VectorXd> joint_angles_vector;
+
+    ee_jac_base_num.resize(kinematic_model->numEE);
+    ee_jac_base.resize(kinematic_model->numEE);
+    joint_angles_vector.resize(kinematic_model->numEE);
+
+    for (int i = 0; i < kinematic_model->numEE; ++i) {
+      ee_jac_base.at(i).resize(3, kinematic_model->GetNumDof(i));
+      ee_jac_base_num.at(i).resize(3, kinematic_model->GetNumDof(i));
+      joint_angles_vector.at(i).resize(kinematic_model->GetNumDof(i));
+    }
+
+    int num_failed = 0;
+    for (int n = 0; n < numTests; ++n) {
+
+      //iterate over the limbs
+      bool failed = false;
+      for (int i = 0; i < kinematic_model->numEE; ++i) {
+
+        joint_angles_vector.at(i).setRandom() * 10;
+        kinematic_model->UpdateModel(joint_angles_vector.at(i), i);
+
+        std::function<Eigen::Vector3d(int)> func_to_evaluate;
+
+        if (which_test == "trans_test") {
+          func_to_evaluate = [=](int limbId) {
+            return kinematic_model->GetEEPositionsBase(limbId);
+          };
+
+          ee_jac_base.at(i) = kinematic_model->GetTranslationalJacobiansWRTjointsBase(i);
+
+        }
+
+        if (which_test == "rot_test") {
+          func_to_evaluate = [=](int limbId) {
+            return kinematic_model->GetEEOrientationBase(limbId);
+          };
+
+          ee_jac_base.at(i) = kinematic_model->GetOrientationJacobiansWRTjointsBase(i);
+
+        }
+
+        ee_jac_base_num.at(i) = getJacobianNumerically(kinematic_model.get(), i,
+                                                       joint_angles_vector.at(i), func_to_evaluate);
+
+        Eigen::MatrixXd res = ee_jac_base.at(i) - ee_jac_base_num.at(i);
+        double max_discrepancy = res.lpNorm<Eigen::Infinity>();
+        if (max_discrepancy > 1e-3) {
+          std::cout << "Big discrepancy in the jacobian for the limb: " << i << std::endl;
+          std::cout << "Analytical: \n" << ee_jac_base.at(i) << std::endl;
+          std::cout << "Numerical: \n" << ee_jac_base_num.at(i) << std::endl;
+          std::cout << "Joints: " << joint_angles_vector.at(i).transpose() << std::endl;
+          std::cout << "position/Orientation: " << func_to_evaluate(i).transpose() << std::endl;
+          std::cout << "\n Max discrepancy: " << max_discrepancy << std::endl << std::endl;
+          failed = true;
+        }
+      }
+
+      if (failed) {
+        ++num_failed;
+      }
+
+    }
+
+    std::cout << "\n=================================================== \n";
+
+    if (which_test == "trans_test")
+      std::cout << "Translation jacobian, tests passed: " << (numTests - num_failed) << "/"
+          << numTests << std::endl;
+
+    if (which_test == "rot_test")
+      std::cout << "Rotation jacobian, tests passed: " << (numTests - num_failed) << "/" << numTests
+          << std::endl;
+    std::cout << "=================================================== \n";
+
+}
+
 int main(int argc, char** argv)
 {
 
   ros::init(argc, argv, "m545_example_node");
+  //seed std library random generator
+  srand((unsigned int) time(nullptr));
   ros::NodeHandle nh;
 
-  //get the excavator model
-  std::string urdfDescription;
-  nh.getParam("romo_mm_description", urdfDescription);
-
-  std::string which_test;
-  nh.getParam("which_test", which_test);
-
-
-  constexpr double dt = 0.01;
-  auto kinematic_model = std::make_shared<M545KinematicModelWithJoints>(urdfDescription, dt);
-
-   //seed std library random generator
-  srand((unsigned int) time(nullptr));
-
-  std::vector<Eigen::MatrixXd> ee_jac_base_num;
-  M545KinematicModelWithJoints::EEJac ee_jac_base;
-  std::vector<Eigen::VectorXd> joint_angles_vector;
-
-  ee_jac_base_num.resize(kinematic_model->numEE);
-  ee_jac_base.resize(kinematic_model->numEE);
-  joint_angles_vector.resize(kinematic_model->numEE);
-
-  for (int i = 0; i < kinematic_model->numEE; ++i) {
-    ee_jac_base.at(i).resize(3, kinematic_model->GetNumDof(i));
-    ee_jac_base_num.at(i).resize(3, kinematic_model->GetNumDof(i));
-    joint_angles_vector.at(i).resize(kinematic_model->GetNumDof(i));
-  }
-
-  int numTests = 10000;
-  nh.getParam("num_tests", numTests);
-  int num_failed = 0;
-  for (int n = 0; n < numTests; ++n) {
-
-    //iterate over the limbs
-    bool failed = false;
-    for (int i = 0; i < kinematic_model->numEE; ++i) {
-
-      joint_angles_vector.at(i).setRandom() * 10;
-      kinematic_model->UpdateModel(joint_angles_vector.at(i), i);
-
-      std::function<Eigen::Vector3d(int)> func_to_evaluate;
-
-      if (which_test == "trans_test") {
-        func_to_evaluate = [=](int limbId) {
-          return kinematic_model->GetEEPositionsBase(limbId);
-        };
-
-        ee_jac_base.at(i) = kinematic_model->GetTranslationalJacobiansWRTjointsBase(i);
-
-      }
-
-      if (which_test == "rot_test") {
-        func_to_evaluate = [=](int limbId) {
-          return kinematic_model->GetEEOrientationBase(limbId);
-        };
-
-        ee_jac_base.at(i) = kinematic_model->GetOrientationJacobiansWRTjointsBase(i);
-
-      }
-
-      ee_jac_base_num.at(i) = getJacobianNumerically(kinematic_model.get(), i,
-                                                     joint_angles_vector.at(i), func_to_evaluate);
-
-      Eigen::MatrixXd res = ee_jac_base.at(i) - ee_jac_base_num.at(i);
-      double max_discrepancy = res.lpNorm<Eigen::Infinity>();
-      if (max_discrepancy > 1e-3) {
-        std::cout << "Big discrepancy in the jacobian for the limb: " << i << std::endl;
-        std::cout << "Analytical: \n" << ee_jac_base.at(i) << std::endl;
-        std::cout << "Numerical: \n" << ee_jac_base_num.at(i) << std::endl;
-        std::cout << "Joints: " << joint_angles_vector.at(i).transpose() << std::endl;
-        std::cout << "position/Orientation: " << func_to_evaluate(i).transpose() << std::endl;
-        std::cout << "\n Max discrepancy: " << max_discrepancy << std::endl << std::endl;
-        failed = true;
-      }
-    }
-
-    if (failed) {
-      ++num_failed;
-    }
-
-  }
-
-  std::cout << "\n=================================================== \n";
-
-  if (which_test == "trans_test")
-    std::cout << "Translation jacobian, tests passed: " << (numTests - num_failed) << "/"
-        << numTests << std::endl;
-
-  if (which_test == "rot_test")
-    std::cout << "Rotation jacobian, tests passed: " << (numTests - num_failed) << "/" << numTests
-        << std::endl;
-  std::cout << "=================================================== \n";
-
-//  int i = 0;
-//  std::cout << "Big discrepancy in the jacobian for the limb: " << i << std::endl;
-//  std::cout << "Analytical: \n" << ee_jac_base.at(i) << std::endl;
-//  std::cout << "Numerical: \n" << ee_jac_base_num.at(i) << std::endl;
-//  std::cout << "Joints: " << joint_angles_vector.at(i).transpose() << std::endl;
+  runTest(nh, "trans_test", 10000);
+  runTest(nh, "rot_test", 10000);
 
   return 0;
 
